a5c9699b7fc0d6d51ef8c2f9f422ea780a45afb8
yonahzag <141697557+yonahzag@users.noreply.github.com>
2023-09-03 04:02:37 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "welcome to setup!"
+echo "Loading aliases."
+
+echo "alias cl='clear'" > ~/.bash_aliases
+echo "alias gtg='exit'" >> ~/.bash_aliases
+
+echo "checking what shell is in use"
+FILE=~/.zshrc
+if test -f "$FILE"; then
+    echo "zsh"
+    echo "adding aliases"
+    echo "source .bash_aliases" >> ~/.zshrc
+fi
+FILE=~/.bashrc
+if test -f "$FILE"; then
+   echo "bash"
+   echo "adding aliases"
+   echo "source .bash_aliases" >> ~/.bashrc
+fi
+sleep 2
+clear
+echo "loading wifi script"
+mkdir ~/.scripts
+mv wifi.sh ~/.scripts
+echo "please restart terminal for changes to take effect."
