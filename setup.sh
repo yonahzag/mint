bfb9a02b5f1368146259aa3dd23427fd12d04c81
yonahzag <141697557+yonahzag@users.noreply.github.com>
2023-11-03 11:56:40 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-echo "welcome to setup!"
-echo "Loading aliases."
+echo "System setting up"
+echo ""
+echo "this script requires root access"
+sudo
+# Setting up aliases 
+
+echo "making .bash_aliases"
+touch ~/.bash_aliases
+echo "setting up aliases"
 
 echo "alias cl='clear'" > ~/.bash_aliases
 echo "alias gtg='exit'" >> ~/.bash_aliases
+echo "alias ls='ls -a'" >> ~/.bash_aliases
+echo "alias install='apt install'" >> ~/.bash_aliases
+echo "alias remove='apt autoremove'"
 
 echo "checking what shell is in use"
 FILE=~/.zshrc
@@ -11,16 +21,24 @@ if test -f "$FILE"; then
     echo "zsh"
     echo "adding aliases"
     echo "source .bash_aliases" >> ~/.zshrc
+    source .bash_aliases
 fi
 FILE=~/.bashrc
 if test -f "$FILE"; then
    echo "bash"
    echo "adding aliases"
    echo "source .bash_aliases" >> ~/.bashrc
+   source .bash_aliases
 fi
-sleep 2
-clear
-echo "loading wifi script"
-mkdir ~/.scripts
-mv wifi.sh ~/.scripts
-echo "please restart terminal for changes to take effect."
+
+# Setting background
+
+echo "Making background folder"
+cd ~/whitedesk/Pictures/
+mkdir backgrounds
+cd backgrounds
+echo "Downloading background"
+touch background.jpg
+curl https://cdni.iconscout.com/illustration/premium/thumb/mountains-at-night-3864545-3318134.png?f=webp -o background.jpg -s
+echo "Setting background"
+gsettings set org.gnome.desktop.background picture-uri file:///home/whitedesk/Pictures/backgrounds/background.jpg
